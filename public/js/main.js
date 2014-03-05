05f917e1f4149963895c3c69f14730005b1cd3bb
Michael Noe <mikejnoe@gmail.com>
2014-03-05 11:50:40 -0500

    Initial tweaks to hookup to Jim's pusher

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,22 @@
+
 $(document).ready(function() {
+});
 
-  // Place JavaScript code here...
+//Enable pusher logging - don't include this in production
+//var Pusher = require('pusher');
 
+Pusher.log = function(message)
+{
+	if (window.console && window.console.log) {
+		window.console.log(message);
+	}
+};
+
+var pusher = new Pusher('a249e105003602fad09c');
+var channel = pusher.subscribe('primedisplay');
+channel.bind('primedisplay_monitor', function(data)
+{
+	//alert(data.message);
+	html = document.getElementById("status_data").innerHTML;
+	document.getElementById("status_data").innerHTML = html + "<br>>" + data.message;
 });
