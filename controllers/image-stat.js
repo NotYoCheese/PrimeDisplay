c88acbce914826ea54b8d6d429d60e4c940e8e4a
Michael Holt <maholt@Peters-Work-MacBook-Pro.local>
2014-05-08 10:11:53 -0400

    Added pagination to Image Stats page

--- controllers/image-stat.js
+++ controllers/image-stat.js
@@ -6,10 +6,56 @@
 
 var ImageStat = require('../models/image-stat');
 
+
 exports.getImageStat = function(req, res)
 {
+	var page = (req.param('page') > 0 ? req.param('page') : 1) - 1
+	var perPage = 3
+	var options = {
+		perPage: perPage,
+		page: page
+	}
+
+	ImageStat.list(options, function(err, results)
+	{
+		if (err) return res.render('500')
+		ImageStat.count().exec(function (err, count)
+		{
+			res.render('image-stat',
+			{
+				_pd_allImages: results,
+				page: page + 1,
+				pages: Math.ceil(count / perPage),
+createPagination: function (pages, page) {
+	var url = require('url')
+	, qs = require('querystring')
+	, params = qs.parse(url.parse(req.url).query)
+	, str = '<ul class="pagination">'
+
+	params.page = 0
+	var clas = page == 0 ? "active" : "no"
+	str += '<li class="' + clas + '"><a href="?' + qs.stringify(params) + '">First</a></li>'
+	for (var p = 1; p < pages; p++)
+	{
+		params.page = p
+		clas = page == p ? "active" : "no"
+		str += '<li class="' + clas + '"><a href="?' + qs.stringify(params) + '">' +  p  + '</a></li>'
+	}
+	params.page = --p
+	clas = page == params.page ? "active" : "no"
+	str += '<li class="' + clas + '"><a href="?' + qs.stringify(params) + '">Last</a></li></ul>'
+
+	return str
+}
+			})
+		})
+	});
+
+/*
         ImageStat
 	.find()
+	.limit(perPage)
+	.skip(perPage * page)
 	.exec(function(err, results)
         {
                 if (err)
@@ -19,9 +65,12 @@ exports.getImageStat = function(req, res)
                 }
                 else
                 {
-                        res.render('image-stat', {_pd_allImages: results});
+                        res.render('image-stat', {
+				_pd_allImages: results
+			});
                 }
         });
+*/
 };
 
 exports.getImageStatServed = function(req, res)
