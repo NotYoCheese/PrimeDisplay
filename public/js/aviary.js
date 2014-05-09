f76dfed267aee4ad0823b850bfaf3f8015bda869
Michael Noe <mikejnoe@gmail.com>
2014-05-09 08:02:36 -0400

    added flash to jquery code

--- public/js/aviary.js
+++ public/js/aviary.js
@@ -1,58 +1,93 @@
+var showAlert = function(type, message) {
 
+  // class - success
+  // alert-danger
+  var alert = $('<div class="alert alert-danger animated fadeIn"> <button type="button" data-dismiss="alert" class="close">×</button> <div>Image not saved to PrimeDisplay: Error: ETIMEDOUT</div></div>');
+//   var messageDiv = document.createElement("div");
 
-function post_to_url(path, params, method) {
-    method = method || "post"; // Set method to post by default if not specified.
+ 
 
-    // The rest of this code assumes you are not using a library.
-    // It can be made less wordy if you use one.
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", path);
+  $('#flash').append(alert);
 
-    for(var key in params) {
-        if(params.hasOwnProperty(key)) {
-            var hiddenField = document.createElement("input");
-            hiddenField.setAttribute("type", "hidden");
-            hiddenField.setAttribute("name", key);
-            hiddenField.setAttribute("value", params[key]);
+// <button type="button" data-dismiss="alert" class="close">×</button>
+//     for(var key in params) {
+//         if(params.hasOwnProperty(key)) {
+//             var hiddenField = document.createElement("input");
+//             hiddenField.setAttribute("type", "hidden");
+//             hiddenField.setAttribute("name", key);
+//             hiddenField.setAttribute("value", params[key]);
 
-            form.appendChild(hiddenField);
-         }
-    }
+//             form.appendChild(hiddenField);
+//          }
+//     }
 
-    document.body.appendChild(form);
-    console.log('calling form.submit()');
-    form.submit();
-}
+};
+
+var updatePDServer = function(path, params, method) {
+  method = method || "post"; // Set method to post by default if not specified.
+  //var data = JSON.stringify(params);
+  var data = params;
+
+  var request = $.ajax({
+    url: '/api/aviary',
+    method: method,
+    timeout: 3000,
+    data: data
+  });
+  // callback handler that will be called on success
+  request.done(function (response, textStatus, jqXHR){
+      // log a message to the console
+      showAlert('success', 'yay');
+      console.log("Hooray, it worked!");
+      console.log("response: ");
+  });
+
+  // callback handler that will be called on failure
+  request.fail(function (jqXHR, textStatus, errorThrown){
+      // log the error to the console
+      console.error(
+          "The following error occured: "+
+          textStatus, errorThrown
+      );
+  });
+
+  // callback handler that will be called regardless
+  // if the request failed or succeeded
+  request.always(function () {
+      // reenable the inputs
+      console.log('all done.')
+  });
+};
 
 var featherEditor = new Aviary.Feather({
-	apiKey: 'c83c98c0da041785',
-	apiVersion: 3,
-	theme: 'dark',
-	tools: 'all',
-	appendTo: '',
-    onSave: function(imageID, newURL) {
-        console.log('new Url: \''+newURL+'\'');
-		$('#'+imageID).attr('src', newURL);
-        params = {
-            _csrf:$('#_csrf').attr('value'),
-            urlToReplace: $('#'+imageID).attr('data-rawurl'),
-            editedUrl: newURL,
-            user: $('#user').attr('value'),
-        };
-        console.log('calling post_to_url');
-        post_to_url('/api/aviary', params);
-	},
-	onError: function(errorObj) {
-		alert(errorObj.message);
-	}
+  apiKey: 'c83c98c0da041785',
+  apiVersion: 3,
+  theme: 'dark',
+  tools: 'all',
+  appendTo: '',
+  onSave: function(imageID, newURL) {
+    console.log('new Url: \''+newURL+'\'');
+    $('#'+imageID).attr('src', newURL);
+    var params = {
+      _csrf:$('#_csrf').attr('value'),
+      urlToReplace: $('#'+imageID).attr('data-rawurl'),
+      editedUrl: newURL,
+      user: $('#user').attr('value'),
+    };
+    console.log('calling updatePDServer');
+    updatePDServer('/api/aviary', params);
+  },
+  onError: function(errorObj) {
+    alert(errorObj.message);
+  }
 });
 
 var launchEditor = function(event) {
-    console.log('launching editor');
-	featherEditor.launch({
-		image: event.data.pdImage._id,
-		url: event.data.pdImage.raw_url
-	});
-	return false;
+  tmpUrl = event.data.pdImage.raw_url.replace('10.10.1.108', 'direct.coinsociety.com');
+  console.log('launching editor');
+  featherEditor.launch({
+    image: event.data.pdImage._id,
+    url: tmpUrl
+  });
+  return false;
 };
